a1b2b377c6fcdc28f27169aa2595ce0e51c1f0e5
Jonanthan Hsu <hsuchun1@ecs.vuw.ac.nz>
2022-05-13 21:40:25 +1200

    Camera class template

---
+++ Camera.cpp
@@ -0,0 +1,36 @@
+class Camera{
+	private int num_black;
+	private int num_red;
+	private int num_blue;
+	private int num_green;
+	
+	public:
+	Camera();
+	double error;
+	double double kp;
+	bool noBlackpix(){
+		
+	}
+	double getLineErrorSignal(){
+		
+	}
+	double numBlackInImg(){
+		
+	}
+	double numRedInImg(){
+		
+	}
+	double numBlueInImg(){
+		
+	}
+	double numGreenInImg(){
+		
+	}
+}
+Camera::Camera(){
+	//intialise
+}
+
+int main(){
+	Camera camera;
+}
