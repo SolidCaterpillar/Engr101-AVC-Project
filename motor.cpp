c19bb333603506771eccedac12f17aa36aa7f920
Jonanthan Hsu <hsuchun1@ecs.vuw.ac.nz>
2022-05-13 21:40:45 +1200

    Motor class template

---
+++ motor.cpp
@@ -0,0 +1,32 @@
+class Motor{
+	private int speedright;
+	private speedleft;
+	
+	public:
+	Motor();
+	double calculateSpeedright(){
+		
+	}
+	double calculateSpeedleft(){
+		
+	}
+	void setSpeedright(){
+		
+	}
+	void setSpeedleft(){
+		
+	}
+	void foward(){
+		
+	}
+	void reverse(){
+		
+	}
+}
+Motor::Motor(){
+	//intialise
+}
+
+int main(){
+	Motor motor;
+}
